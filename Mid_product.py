c2c0d4ab3cce9c5a77fe52a51c3169c0fd733784
THSayeb <desktop499@gmail.com>
2024-12-27 23:56:56 +0600

    Nested loops

--- Mid_product.py
+++ Mid_product.py
@@ -1,31 +1,26 @@
-num = int(input("Enter a number : "))
-t = num 
-j = num
-numlen = 0
+#Input a number 
+num = int(input("Enter the number : "))
+t = num
+numLen = 0
+#iterate the loop
+while t>0: 
+  numLen = numLen+1
+  t = int(t/10)
 
-while t > 0 or t < -1:
-    numlen = numlen+1 
-    t = int(t/10)
-
-if numlen >= 4:
-    numlen = int(numlen/2)
-    chk = 0 
-
-    while num > 0 or num < -1:
-        rem = num%10
-
-        if chk == numlen:
-            midone = rem
-        elif chk == (numlen-1):
-            midtwo = rem
-        num = int(num / 10)
-        chk = chk + 1
-    prod = midone * midtwo
-
-    print("\nProducts of mid digits (", str(midone), "*", str(midtwo), ") =", prod)
+if numLen>=4: #condition 1
+  numLen = int(numLen/2)
+  chk = 0
+  while num>0: #iterate loop
+    rem = num%10
+    if chk==numLen: #nested condition 1
+      midOne = rem
+    elif chk==(numLen-1): 
+      midTwo = rem
+    num = int(num/10)
+    chk = chk+1
+  prod = midOne*midTwo #product of middle digits
+  #display the result
+  print("\nProduct of Mid digits (" +str(midOne)+ "*" +str(midTwo)+ ") = ", prod)
 
 else:
-    print("\nIt's not a 4 or more than 4-digit number!")
-
-
-        
+  print("\nIt's not a 4 or more than 4-digit number!")
