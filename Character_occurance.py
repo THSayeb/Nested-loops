03590aa1b28c48cd8e7dd71e930967fb0a13b364
THSayeb <desktop499@gmail.com>
2024-12-27 21:03:50 +0600

    Character_occurance

---
+++ Character_occurance.py
@@ -0,0 +1,13 @@
+string = input("Enter your word ")
+cha = input("Enter the specific number ")
+
+i = 0
+count = 0
+
+while i < len(string):
+    j = 0
+    if string[i] == cha:
+        count += 1
+    i += 1
+
+print("'",string, "'", " has ", count, "'", cha, "'")
